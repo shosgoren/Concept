6b1eb8704eb1835afdf732e3a2a91f7170457918
anlasotomasyon <42038461+anlasotomasyon@users.noreply.github.com>
2018-08-06 22:58:40 +0300

    Initial commit

---
+++ FaysConcept.BackOffice/Kasalar/FrmKasa.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using FaysConcept.BackOffice.Cari;
+using FaysConcept.Entities.Context;
+using FaysConcept.Entities.DataAccess;
+
+namespace FaysConcept.BackOffice.Kasalar
+{
+    public partial class FrmKasa : DevExpress.XtraEditors.XtraForm
+    {
+        KasaDAL kasaDal=new KasaDAL();
+        FaysConceptContext context=new FaysConceptContext();
+
+        public FrmKasa()
+        {
+            InitializeComponent();
+        }
+
+        public void Guncelle()
+        {
+         gridControlKasa.DataSource= kasaDal.KasaListele(context);
+        }
+
+        private void btnguncelle_Click(object sender, EventArgs e)
+        {
+            Guncelle();
+        }
+
+        private void btnkapat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnekle_Click(object sender, EventArgs e)
+        {
+            FrmKasaIslem form = new FrmKasaIslem(new Entities.Tables.Kasa());
+            form.ShowDialog();
+            if (form.Kaydedildi)
+            {
+                Guncelle();
+
+            }
+        }
+
+        private void btnduzenle_Click(object sender, EventArgs e)
+        {
+            secilen = layoutView1.GetFocusedRowCellValue(colKasaKodu).ToString();
+            FrmKasaIslem form = new FrmKasaIslem(kasaDal.GetByFilter(context, c => c.KasaKodu == secilen));
+            form.ShowDialog();
+            if (form.Kaydedildi) // kapat butununa basıldığında gereksiz yere güncelleme işlemi yapmamak için ; form saved metodu (cariislem kaydet butonu altında) (addorrupdate) true ise listeyi güncelle
+            {
+                Guncelle();
+
+            }
+        }
+    }
+}
---
+++ FaysConcept.Entities/DataAccess/KasaDAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FaysConcept.Entities.Context;
+using FaysConcept.Entities.Repositories;
+using FaysConcept.Entities.Tables;
+using FaysConcept.Entities.Validations;
+
+namespace FaysConcept.Entities.DataAccess
+{
+   public class KasaDAL : EntityRepositoryBase<FaysConceptContext, Kasa,KasaValidator>
+    {
+        public object KasaListele(FaysConceptContext context)
+        {
+            var result = context.Kasalar.GroupJoin(context.KasaHareketleri, c => c.KasaKodu, c => c.KasaKodu,
+                (kasa, kasahareket) => new
+                {
+                    kasa.Id,
+                    kasa.KasaKodu,
+                    kasa.KasaAdi,
+                    kasa.YetkiliKodu,
+                    kasa.YetkiliAdi,
+                    kasa.Aciklama,
+                    KasaGiris = (kasahareket.Where(c => c.KasaKodu == kasa.KasaKodu && c.Hareket == "Kasa Giris").Sum(c => c.Tutar) ?? 0),
+                    KasaCikis = (kasahareket.Where(c => c.KasaKodu == kasa.KasaKodu && c.Hareket == "Kasa Cikis").Sum(c => c.Tutar) ?? 0),
+                    Bakiye = (kasahareket.Where(c => c.KasaKodu == kasa.KasaKodu && c.Hareket == "Kasa Giris").Sum(c => c.Tutar) ?? 0) -
+                             (kasahareket.Where(c => c.KasaKodu == kasa.KasaKodu && c.Hareket == "Kasa Cikis").Sum(c => c.Tutar) ?? 0)
+                }).ToList();
+            return result;
+        }
+    }
+}
